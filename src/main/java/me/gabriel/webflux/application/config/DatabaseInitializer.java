ec9643d9e05c7124c4426281563792b65fe920b5
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-01 09:00:49 -0400

    chore: adiciona massa de dados inicial

---
+++ src/main/java/me/gabriel/webflux/application/config/DatabaseInitializer.java
@@ -0,0 +1,33 @@
+package me.gabriel.webflux.application.config;
+
+import lombok.AllArgsConstructor;
+import me.gabriel.webflux.data.db.mongo.document.PlaylistDocument;
+import me.gabriel.webflux.data.db.mongo.repositories.ReactivePlaylistRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+/**
+ * @author daohn
+ * @since 01/08/2021
+ */
+@Configuration
+@AllArgsConstructor
+public class DatabaseInitializer implements CommandLineRunner {
+
+  private final ReactivePlaylistRepository playlistRepository;
+
+  @Override public void run(String... args) {
+
+    playlistRepository.deleteAll()
+      .thenMany(
+        Flux.just("API REST Spring Boot", "Deploy de uma aplicação java no IBM Cloud", "Java 8",
+                  "Github", "Spring Security", "Web Service RESTFULL", "Bean no Spring Framework"
+          )
+          .map(name -> new PlaylistDocument(UUID.randomUUID().toString(), name))
+          .flatMap(playlistRepository::save)
+      ).subscribe(System.out::println);
+  }
+}
